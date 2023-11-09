baf07326e9de7b512368b23a5d065a54121485fe
Maximesol <maxime.gognies@gmail.com>
2023-11-09 16:53:06 +0100

    🚧 Main > Add currentWorkflowStatus state

--- frontend/app/components/Main.jsx
+++ frontend/app/components/Main.jsx
@@ -1,15 +1,17 @@
+'use client'
 import { Flex } from "@chakra-ui/react";
+import {useState} from "react"
 import Nav from "./Nav";
 import VoteProgressIndicator from "./VoteProgressIndicator";
 import ContractManager from "./ContractManager";
 import ContractInfoDisplay from "./ContractInfoDisplay";
 
 export default function Main() {
-    const currentStage = 4;
+    const [currentWorkflowStatus, setcurrentWorkflowStatus] = useState(5) // state pour le workflowStatus
   return (
     <Flex direction="column" h="100vh">
       <Nav />
-      <VoteProgressIndicator currentStage = {currentStage} />
+      <VoteProgressIndicator currentWorkflowStatus = {currentWorkflowStatus} />
       <Flex flex="1">
         <ContractManager flex="2" />
         <ContractInfoDisplay flex="1" />
