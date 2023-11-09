0ebec241ca2826ddb28fee4c278e6be2fae9f2de
Maximesol <maxime.gognies@gmail.com>
2023-11-09 16:53:41 +0100

    ✨ VoteProgressIndicator > Add Stepper

--- frontend/app/components/VoteProgressIndicator.jsx
+++ frontend/app/components/VoteProgressIndicator.jsx
@@ -1,27 +1,63 @@
-import { Flex, Progress, Text, Box } from "@chakra-ui/react";
+'use client'
+import {
+  Step,
+  StepDescription,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  Stepper,
+  useSteps, 
+  Box,
+  VStack,
+  Container
+} from '@chakra-ui/react'
 
-export default function VoteProgressIndicator({currentStage}) {
-  const workflowStages = [
-    "Registering Voters",
-    "Proposals Registration Started",
-    "Proposals Registration Ended",
-    "Voting Session Started",
-    "Voting Session Ended",
-    "Votes Tallied"
-  ];
-  
 
+const steps = [
+  { title: '1', description: 'Registering Voters' },
+  { title: '2', description: 'Proposals Registration Started' },
+  { title: '3', description: 'Proposals Registration Ended' },
+  { title: '4', description: 'Voting Session Started' },
+  { title: '5', description: 'Voting Session Ended' },
+  { title: '6', description: 'Votes Tallied' },
+
+]
+
+export default function VoteProgressIndicator({currentWorkflowStatus}) {
+  const { activeStep } = useSteps({
+    index: currentWorkflowStatus,
+    count: steps.length,
+  })
 
   return (
-    <Flex direction="column" align="center" mt={10}>
-      <Progress value={(currentStage / (workflowStages.length - 1)) * 100} width="80%" height="32px" />
-      <Box display="flex" justifyContent="space-between" width="80%" mt={2}>
-        {workflowStages.map((stage, index) => (
-          <Text fontSize="sm" key={index} color={index <= currentStage ? "blue.500" : "gray.500"}>
-            {stage}
-          </Text>
-        ))}
-      </Box>
-    </Flex>
+
+  <Box marginTop="6" marginBottom="6" marginRight="20" marginLeft="20">
+    <Stepper size='md' index={activeStep}>
+      {steps.map((step, index) => (
+        <Step key={index}>
+          <VStack spacing={2} align="center"> {/* Utilisez VStack pour un alignement vertical */}
+            <StepIndicator>
+              <StepStatus
+                complete={<StepIcon />}
+                incomplete={<StepNumber />}
+                active={<StepNumber />}
+              />
+            </StepIndicator>
+
+            <Box textAlign="center">
+              <StepTitle fontSize="sm">{step.title}</StepTitle>
+              <StepDescription fontSize="xs">{step.description}</StepDescription>
+            </Box>
+          </VStack>
+
+          <StepSeparator />
+        </Step>
+      ))}
+    </Stepper>
+    </Box>
+  
   );
 }
