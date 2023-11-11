9ef134e79c4ae9c08d38f739207851248d8d64d0
Maximesol <maxime.gognies@gmail.com>
2023-11-11 14:50:43 +0100

    🐛 VoteProgressIndicator > Correction du bug d'affichage, je suppose que useSteps de chakra ui fonctionne bizarrement donc j'ai contourné la chose

--- frontend/app/components/VoteProgressIndicator.jsx
+++ frontend/app/components/VoteProgressIndicator.jsx
@@ -6,58 +6,52 @@ import {
   StepIndicator,
   StepNumber,
   StepSeparator,
-  StepStatus,
   StepTitle,
   Stepper,
-  useSteps, 
   Box,
   VStack,
-  Container
-} from '@chakra-ui/react'
-
+  StepStatus
+} from '@chakra-ui/react';
 
 const steps = [
-  { title: '1', description: 'Registering Voters' },
-  { title: '2', description: 'Proposals Registration Started' },
-  { title: '3', description: 'Proposals Registration Ended' },
-  { title: '4', description: 'Voting Session Started' },
-  { title: '5', description: 'Voting Session Ended' },
-  { title: '6', description: 'Votes Tallied' },
-
-]
+  { title: '0', description: 'Registering Voters' },
+  { title: '1', description: 'Proposals Registration Started' },
+  { title: '2', description: 'Proposals Registration Ended' },
+  { title: '3', description: 'Voting Session Started' },
+  { title: '4', description: 'Voting Session Ended' },
+  { title: '5', description: 'Votes Tallied' },
+];
 
 export default function VoteProgressIndicator({workflowStatus}) {
-  const { activeStep } = useSteps({
-    index: workflowStatus,
-    count: steps.length,
-  })
+  console.log("workflowStatus dans VoteProgressIndicator: ", workflowStatus);
+
+  // Utilisez directement workflowStatus comme activeStep
+  const activeStep = workflowStatus;
 
   return (
+    <Box marginTop="6" marginBottom="6" marginRight="20" marginLeft="20">
+      <Stepper size='md' index={activeStep}>
+        {steps.map((step, index) => (
+          <Step key={index}>
+            <VStack spacing={2} align="center">
+              <StepIndicator>
+                <StepStatus
+                  complete={<StepIcon />}
+                  incomplete={<StepNumber />}
+                  active={<StepNumber />}
+                />
+              </StepIndicator>
 
-  <Box marginTop="6" marginBottom="6" marginRight="20" marginLeft="20">
-    <Stepper size='md' index={activeStep}>
-      {steps.map((step, index) => (
-        <Step key={index}>
-          <VStack spacing={2} align="center"> {/* Utilisez VStack pour un alignement vertical */}
-            <StepIndicator>
-              <StepStatus
-                complete={<StepIcon />}
-                incomplete={<StepNumber />}
-                active={<StepNumber />}
-              />
-            </StepIndicator>
+              <Box textAlign="center">
+                <StepTitle fontSize="sm">{step.title}</StepTitle>
+                <StepDescription fontSize="xs">{step.description}</StepDescription>
+              </Box>
+            </VStack>
 
-            <Box textAlign="center">
-              <StepTitle fontSize="sm">{step.title}</StepTitle>
-              <StepDescription fontSize="xs">{step.description}</StepDescription>
-            </Box>
-          </VStack>
-
-          <StepSeparator />
-        </Step>
-      ))}
-    </Stepper>
+            <StepSeparator />
+          </Step>
+        ))}
+      </Stepper>
     </Box>
-  
   );
 }
