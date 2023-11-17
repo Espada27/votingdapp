7585082c0e89329fca7ac27815c0a89ec2e9696c
espada27 <jonathandugard@gmail.com>
2023-11-17 18:26:17 +0100

    Disable addProposal text and button while loading

--- frontend/app/components/DisplayAddProposal.jsx
+++ frontend/app/components/DisplayAddProposal.jsx
@@ -1,33 +1,45 @@
-'use client'
-import { VStack, Text, Input, Button } from '@chakra-ui/react';
-import { useState, useContext } from 'react';
-import ContractContext from '../context/ContractContext';
-
+"use client";
+import { VStack, Text, Input, Button } from "@chakra-ui/react";
+import { useState, useContext } from "react";
+import ContractContext from "../context/ContractContext";
 
 export default function DisplayAddProposal() {
+  const { addProposal } = useContext(ContractContext);
 
-    const { addProposal } = useContext(ContractContext);
+  const [newProposal, setNewProposal] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [newProposal, setNewProposal] = useState('');
-
-
-    const handleAddProposal = () => {
-        if (newProposal) {
-            addProposal(newProposal);
-            setNewProposal('')
-        }
+  const handleAddProposal = async () => {
+    if (!newProposal || isLoading) {
+      return;
     }
+    setIsLoading(true);
+
+    await addProposal(newProposal).finally(() => {
+      setIsLoading(false);
+    });
+
+    setNewProposal("");
+  };
 
   return (
     <VStack spacing={4}>
-        <Text>Ajouter une Proposal :</Text>
-        <Input 
-            placeholder="Décrivez votre proposal" 
-            value={newProposal}
-            width= "400px" 
-            onChange={(e) => setNewProposal(e.target.value.toString())} 
-        />
-        <Button colorScheme="blue" onClick={handleAddProposal}>Ajouter</Button>
+      <Text>Ajouter une Proposal :</Text>
+      <Input
+        placeholder="Décrivez votre proposal"
+        value={newProposal}
+        isDisabled={isLoading}
+        width="400px"
+        onChange={(e) => setNewProposal(e.target.value)}
+      />
+      <Button
+        colorScheme="blue"
+        onClick={handleAddProposal}
+        isDisabled={isLoading}
+        isLoading={isLoading}
+      >
+        Ajouter
+      </Button>
     </VStack>
-  )
+  );
 }
