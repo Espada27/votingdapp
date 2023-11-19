c48182972959b6fd743d041bb8f1d11293f28301
espada27 <jonathandugard@gmail.com>
2023-11-19 16:28:19 +0100

    Add loading on addVoter component

--- frontend/app/components/DisplayAddVoter.jsx
+++ frontend/app/components/DisplayAddVoter.jsx
@@ -1,32 +1,44 @@
-'use client'
-import { VStack, Text, Input, Button } from '@chakra-ui/react';
-import { useState, useContext } from 'react';
-import ContractContext from '../context/ContractContext';
-
+"use client";
+import { VStack, Text, Input, Button } from "@chakra-ui/react";
+import { useState, useContext } from "react";
+import ContractContext from "../context/ContractContext";
 
 export default function DisplayAddVoter() {
+  const { addVoter } = useContext(ContractContext);
 
-    const { addVoter } = useContext(ContractContext);
+  const [newVoterAddress, setNewVoterAddress] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [newVoterAddress, setNewVoterAddress] = useState('');
-
-
-    const handleAddVoter = () => {
-        if (newVoterAddress) {
-            addVoter(newVoterAddress);
-            setNewVoterAddress('')
-        }
+  const handleAddVoter = async () => {
+    if (!newVoterAddress) {
+      return;
     }
+    setIsLoading(true);
+    try {
+      await addVoter(newVoterAddress);
+      setNewVoterAddress("");
+    } catch (error) {
+      console.log("Error while adding a new voter", error);
+    }
+    setIsLoading(false);
+  };
 
   return (
     <VStack spacing={4}>
-        <Text>Ajouter un Voter :</Text>
-        <Input 
-             placeholder="Adresse du voter" 
-            value={newVoterAddress} 
-            onChange={(e) => setNewVoterAddress(e.target.value.toString())} 
-        />
-        <Button colorScheme="blue" onClick={handleAddVoter}>Ajouter</Button>
+      <Text>Ajouter un Voter :</Text>
+      <Input
+        placeholder="Adresse du voter"
+        value={newVoterAddress}
+        onChange={(e) => setNewVoterAddress(e.target.value.toString())}
+      />
+      <Button
+        colorScheme="blue"
+        isDisabled={isLoading}
+        isLoading={isLoading}
+        onClick={handleAddVoter}
+      >
+        Ajouter
+      </Button>
     </VStack>
-  )
+  );
 }
